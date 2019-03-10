29e44fe1f6bb92e82dcc2b11e7303d24445895b3
apep <apep.susnendar@qron.co.id>
2019-03-10 20:25:23 +0700

    org chart

---
+++ app/view/organization/OrganizationForm.js
@@ -0,0 +1,56 @@
+Ext.define('qron.view.organization.OrganizationForm', {
+    extend: 'Ext.form.Panel',
+    id: 'frm-org',
+    layout:'anchor',
+    padding:10,
+    frame:true,
+    requires:[
+        'app.ux.form.field.TreeComboBox'
+    ], 
+    initComponent: function() {
+        var me = this;
+
+        var treestore = Ext.create('Ext.data.TreeStore', {
+            fields: [
+                {name: 'name', type: 'string'}
+            ],
+            autoLoad:true,
+            proxy: {
+                type: 'ajax',
+                url: CONFIG.endPointApi + 'v1/organizations',
+                headers: {
+                    'Authorization' : 'Bearer ' + Ext.JSON.decode(localStorage['qronkpiusertoken']).token
+                }
+            },
+            folderSort: false
+        });
+
+        this.items = [
+            {
+                xtype:'textfield',
+                fieldLabel:'Name',
+                anchor:'100%',
+                name:'name',
+                allowBlank: false
+            },
+            Ext.create('app.ux.form.field.TreeComboBox', {
+                fieldLabel: "Parent",
+                name: 'parent',
+                store: treestore,
+                anchor:'100%',
+                queryMode: 'remote',
+                valueField: 'id',
+                displayField: 'name',
+                anyMatch: true,
+                allowFolderSelect: true,
+                treeConfig: {
+                    maxHeight: 200,
+                    scrolling: true,
+                    align:'start'
+                }
+            })
+        ];
+
+        this.callParent(arguments);
+    }
+});
